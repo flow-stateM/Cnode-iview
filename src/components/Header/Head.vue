315d9c34f2b702a4aa2be09c106fadeae23fb61a
flow-stateM <maoshiyude@126.com>
2017-12-26 17:29:40 +0800

    first commit

---
+++ src/components/Header/Head.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <Row>
+      <Col span='12'>
+      12
+      </Col>
+      <Col span = '12'>
+      12
+      </Col>
+    </Row>
+  </header>
+</template>
+
+<script>
+import {Button,Row,Col} from 'iview';
+
+export default {
+  name:'Head',
+  components:{
+    Button,
+    Row,
+    Col
+  }
+}
+</script>
+
---
+++ src/components/Home/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <Head>
+  </Head>
+</template>
+<script>
+import Head from '@/components/Header/Head.vue'
+export default {
+  name:'Home',
+  components:{
+    Head
+  }
+}
+</script>
+
